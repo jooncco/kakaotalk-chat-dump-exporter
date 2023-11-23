eac089f03190dd0f32f292cc887e336d26fa3cbf
jooncco <jooncco.g@gmail.com>
2023-11-23 08:29:40 +0000

    :sparkles: new: exporter main

---
+++ README.md
@@ -0,0 +1,12 @@
+# 모듈 설치 스크립트
+opencv 모듈을 설치하지 않으면 confidence 관련 오류가 납니다.
+```
+pip install opencv-python
+pip install pyperclip
+pip install pyautogui
+```
+
+# 실행 스크립트
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,100 @@
+import pyautogui
+import pyperclip
+import os
+from datetime import datetime
+
+def get_file_name():
+    now= datetime.now()
+    file_name_str= now.strftime("%Y%m%d_%H_%M")
+    return file_name_str
+
+
+def click_img(imagePath):
+    location = pyautogui.locateCenterOnScreen(imagePath, confidence = conf)
+    x, y = location
+    pyautogui.click(x, y, clicks=1)
+
+
+def print_banner():
+    print('==============================')
+    print(' Kakaotalk Chat Dump Exporter')
+    print('                      v0.0.1 ')
+    print('==============================')
+
+
+def print_monitor_spec():
+    print('Monitor ', end='')
+    print(pyautogui.size())
+    print('Mouse ', end='')
+    print(pyautogui.position())
+
+
+def export_chat_dump(file_name):
+    # click menu button
+    try:
+        click_img(img_path + 'menu.png')
+    except:
+        try:
+            click_img(img_path + 'menu_hover.png')
+        except:
+            try:
+                click_img(img_path + 'menu_inactive.png')
+            except:
+                print('Failed: click menu')
+                return False
+
+    # click 'chat dump'
+    try:
+        click_img(img_path + 'chat_dump.png')
+    except:
+        try:
+            click_img(img_path + 'chat_dump_hover.png')
+        except:
+            print('Failed: click chat_dump')
+            return False
+
+    # click 'export chat'
+    try:
+        click_img(img_path + 'export_chat.png')
+    except:
+        try:
+            click_img(img_path + 'export_chat_hover.png')
+        except:
+            print('Failed: export chat')
+            return False
+
+    pyperclip.copy(file_name)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.keyDown('enter')
+    
+    # click confirm button
+    try:
+        click_img(img_path + 'confirm.png')
+    except:
+        try:
+            click_img(img_path + 'confirm_hover.png')
+        except:
+            try:
+                click_img(img_path + 'confirm_inactive.png')
+            except:
+                print('Exit with esc.')
+                pyautogui.keyDown('esc')
+
+    # export success
+    return True
+
+# config
+img_path = os.path.dirname(os.path.realpath(__file__)) + '/img/'
+conf = 0.90
+pyautogui.PAUSE = 0.5
+
+if __name__ == "__main__":
+    print_banner()
+    print_monitor_spec()
+
+    file_name= get_file_name()
+    is_export_success= export_chat_dump(file_name)
+
+    if is_export_success:
+        print()
+        print('Export done. File name: ' + file_name)
